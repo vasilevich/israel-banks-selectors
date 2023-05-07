d068951e14f40b84a599e16ea78513f55f4eb8cc
yosef langer <yosef@doar.systems>
2023-05-07 19:17:49 +0900

    fixed stuff and made form look better

--- js/bundle.js
+++ js/bundle.js
@@ -1,5 +1,25 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (() => {
+  const {validateBankAccount, RESULT} = require("israeli-bank-validation");
+
+  function validateForm() {
+    const bank = jQuery("#bankNumber").val() || "";
+    const department = jQuery("#departmentNumber").val() || "";
+    const account = jQuery("#accountNumber").val() || "";
+
+    const validationResult = validateBankAccount(bank, department, account);
+    if (validationResult === RESULT.VALID) {
+      jQuery("#accountNumber").removeClass("is-invalid");
+      jQuery("#accountNumber").addClass("is-valid");
+      return true;
+    } else {
+      jQuery("#accountNumber").removeClass("is-valid");
+      jQuery("#accountNumber").addClass("is-invalid");
+      return false;
+    }
+  }
+
+  // Initialize the bank data object
   const bankData = {
     bankNumber: 0,
     departmentNumber: 0,
@@ -18,22 +38,23 @@
   const allDeparments = getAllBranches();
 
 // Initialize Select2 for bank and department numbers
-  $(document).ready(function () {
-    $("#bankNumber").select2({
+  jQuery(document).ready(function () {
+    jQuery("#bankNumber").select2({
       placeholder: "Select a bank",
       allowClear: true,
       data: allBanks.map(bank => ({id: bank.bankCode, text: `${bank.bankCode} - ${bank.bankName}`}))
     });
 
-    $("#departmentNumber").select2({
+    jQuery("#departmentNumber").select2({
       placeholder: "Select a department",
       allowClear: true
     });
 
     // Update the department list when a bank is selected
-    $("#bankNumber").on("change", function () {
-      const bankCode = parseInt($(this).val(), 10);
+    jQuery("#bankNumber").on("change", function () {
+      const bankCode = parseInt(jQuery(this).val(), 10);
       bankData.bankNumber = bankCode;
+      validateForm();
       if (bankCode) {
         const departments = allDeparments.filter(department => department.bankCode === bankCode);
         const departmentOptions = departments.map(department => ({
@@ -41,59 +62,49 @@
           text: `${department.branchCode} - ${department.branchName}`
         }));
 
-        $("#departmentNumber").empty().select2({
+        jQuery("#departmentNumber").empty().select2({
           placeholder: "Select a department",
           allowClear: true,
           data: departmentOptions
         }).prop("disabled", false);
       } else {
-        $("#departmentNumber").empty().prop("disabled", true);
+        jQuery("#departmentNumber").empty().prop("disabled", true);
       }
 
-      $("#accountNumber").val("").prop("disabled", true);
-    });
+      jQuery("#accountNumber").val("").prop("disabled", true);
+    })
+      .change();
 
     // Enable the account number input when a department is selected
-    $("#departmentNumber").on("change", function () {
-      const departmentSelected = !!$(this).val();
-      $("#accountNumber").prop("disabled", !departmentSelected);
+    jQuery("#departmentNumber").on("change", function () {
+      const departmentSelected = !!jQuery(this).val();
+      jQuery("#accountNumber").prop("disabled", !departmentSelected);
       bankData.departmentNumber = parseInt(departmentSelected, 10);
-    });
+      validateForm();
+    })
+      .change();
 
     // Validate the account number as the user types
-    $("#accountNumber").on("input", function () {
-      const bankNumber = $("#bankNumber").val();
-      const departmentNumber = $("#departmentNumber").val();
-      const accountNumber = $(this).val();
-      if (bankNumber && departmentNumber && accountNumber) {
-        if (validateBankAccount(bankNumber, departmentNumber, accountNumber) === RESULT.VALID) {
-          $(this).removeClass("is-invalid");
-        } else {
-          $(this).addClass("is-invalid");
-        }
-      } else {
-        $(this).removeClass("is-invalid");
-      }
+    jQuery("#accountNumber").on("input", function () {
+      validateForm();
     });
-    const {validateBankAccount, RESULT} = require("israeli-bank-validation");
     // Display an error popover when the form is submitted with invalid data
-    $("#bankForm").on("submit", function (event) {
+    jQuery("#bankForm").on("submit", function (event) {
       event.preventDefault();
-      const bankNumber = $("#bankNumber").val();
-      const departmentNumber = $("#departmentNumber").val();
-      const accountNumber = $("#accountNumber").val();
+      const bankNumber = jQuery("#bankNumber").val();
+      const departmentNumber = jQuery("#departmentNumber").val();
+      const accountNumber = jQuery("#accountNumber").val();
       if (bankNumber && departmentNumber && accountNumber) {
         bankData.accountNumber = parseInt(accountNumber, 10);
         bankData.valid = false;
-        if (validateBankAccount(bankNumber, departmentNumber, accountNumber) !== RESULT.VALID) {
-          $("#accountNumber").popover({
+        if (!validateForm()) {
+          jQuery("#accountNumber").popover({
             content: "מספר החשבון אינו תקין",
             placement: "top",
             trigger: "manual"
           }).popover("show");
-
           setTimeout(function () {
-            $("#accountNumber").popover("hide");
+            jQuery("#accountNumber").popover("hide");
           }, 3000);
         } else {
           bankData.valid = true;
@@ -102,6 +113,35 @@
     });
 
   });
+
+
+
+  $("form").on("submit", function (event) {
+    event.preventDefault();
+
+    if (!validateForm()) {
+      return false;
+    }
+
+    const bank = $("#bankNumber option:selected").text();
+    const department = $("#departmentNumber option:selected").text();
+    const account = $("#accountNumber").val();
+
+    const successMessage = `
+    <div class="alert alert-success mt-3" role="alert">
+      <i class="bi bi-check-circle-fill"></i> הפרטים נשלחו בהצלחה:
+      <ul>
+        <li><strong>בנק:</strong> ${bank}</li>
+        <li><strong>מחלקה:</strong> ${department}</li>
+        <li><strong>מספר חשבון:</strong> ${account}</li>
+      </ul>
+    </div>
+  `;
+
+    $("#successMessage").remove();
+    $("form").after(successMessage);
+  });
+
 
 })();
 
